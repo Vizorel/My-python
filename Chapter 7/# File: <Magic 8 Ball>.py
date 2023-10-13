43c2ebfc153fd173bb51e30119aa0160a9fa9b9c
Vizorel <122837057+Vizorel@users.noreply.github.com>
2023-10-13 09:59:08 -0700

    Updated # File: <Magic 8 Ball>.py
    I somehow managed to make it work now and will actually anwser the question without reloading the program

--- Chapter 7/# File: <Magic 8 Ball>.py
+++ Chapter 7/# File: <Magic 8 Ball>.py
@@ -1,5 +1,5 @@
 # File: <Magic 8 Ball>
-# Description: <>
+# Description: <a program that simulates a Magic 8 Ball>
 # Assignment Name and Number: #13 of Chapter 7
 #
 # Name: <Zachary Windus>
@@ -7,26 +7,29 @@
 #
 # On my honor, <Zachary Windus>, this programming assignment is my own work
 # and I have not provided this code to any other student.
-
-#important note: I think I broke python becuase if you enter a response it won't answer it but if you click run pytho file it will answer the previous question idk why
-#either that or its just answering before you respond... either way I am so confused and don't know how to fix this error if you could help me that would be great
 import random
 
-responses = 0
-def magic_8_ball(responses):
-    print("Welcome to the Magic 8 Ball!")
-    while True:
-        question = input("Ask the Magic 8 Ball a yes or no question (or type 'quit' to exit): ")
-        if question.lower() == 'quit':
-            break
-        
-        response = random.choice(responses)
-        print(f"Magic 8 Ball says: {response}\n")
-
-def load_responses(filename):
-   with open(filename, 'r') as file:
+# Read responses from the file into a list
+with open('/Users/ZacharyWindus/Downloads/8_ball_responses.txt', 'r') as file:
     responses = file.read().splitlines()
-    return responses
 
-magic_8_ball(responses)
-responses = load_responses("/Users/ZacharyWindus/Downloads/8_ball_responses.txt")
+def magic_8_ball():
+    while True:
+        # Prompt the user to ask a question
+        question = input("Ask the Magic 8 Ball a yes or no question (or 'quit' to exit): ").lower()
+
+        # Check if the user wants to quit
+        if question == 'quit':
+            print("Goodbye!")
+            break
+
+        if '?' not in question:
+            print("Please ask a yes or no question.")
+        else:
+            # Generate a random response from the list of responses
+            random_response = random.choice(responses)
+            print(random_response)
+
+if __name__ == "__main__":
+    print("Welcome to the Magic 8 Ball!")
+    magic_8_ball()
