28ca3a9290292cf2b62d304561b837af827a07d9
Vizorel <122837057+Vizorel@users.noreply.github.com>
2023-10-13 09:59:42 -0700

    Update # File: <Magic 8 Ball>.py

--- Chapter 7/# File: <Magic 8 Ball>.py
+++ Chapter 7/# File: <Magic 8 Ball>.py
@@ -9,16 +9,13 @@
 # and I have not provided this code to any other student.
 import random
 
-# Read responses from the file into a list
+
 with open('/Users/ZacharyWindus/Downloads/8_ball_responses.txt', 'r') as file:
     responses = file.read().splitlines()
 
 def magic_8_ball():
     while True:
-        # Prompt the user to ask a question
         question = input("Ask the Magic 8 Ball a yes or no question (or 'quit' to exit): ").lower()
-
-        # Check if the user wants to quit
         if question == 'quit':
             print("Goodbye!")
             break
@@ -26,7 +23,6 @@ def magic_8_ball():
         if '?' not in question:
             print("Please ask a yes or no question.")
         else:
-            # Generate a random response from the list of responses
             random_response = random.choice(responses)
             print(random_response)
 
